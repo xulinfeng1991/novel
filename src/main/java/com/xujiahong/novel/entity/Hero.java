4d42c4e092ae0c3e3d272eafe5064bd2b2c06af0
xujiahong <xujiahong@lymsh.com>
2018-07-16 16:20:43 +0800

    模板之家，bootstrap后端UI模板引入

---
+++ src/main/java/com/xujiahong/novel/entity/Hero.java
@@ -0,0 +1,13 @@
+package com.xujiahong.novel.entity;
+
+import com.parsec.universal.dao.annot.ParsecTable;
+
+/**
+ * description
+ *
+ * @author xujiahong
+ * @date 2018/7/16
+ */
+@ParsecTable("hero")
+public class Hero {
+}
---
+++ src/main/resources/static/myjs/base.js
@@ -0,0 +1,7 @@
+/*导入头部、尾部、一级导航、二级导航*/
+$(document).ready(function(){
+   $("header").load("header.html");
+   $("footer").load("footer.html");
+   $(".top-nav").load("top-nav.html");
+   $(".sub-nav").load("sub-nav.html");
+});
