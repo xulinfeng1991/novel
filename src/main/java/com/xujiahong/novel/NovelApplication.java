958369a0a88e37f4f3e52a520f4655165d20e677
xujiahong <xujiahong@lymsh.com>
2018-07-16 10:50:53 +0800

    模板之家，bootstrap后端UI模板引入

--- src/main/java/com/xujiahong/novel/NovelApplication.java
+++ src/main/java/com/xujiahong/novel/NovelApplication.java
@@ -2,9 +2,17 @@ package com.xujiahong.novel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class NovelApplication {
+
+	@RequestMapping("/")
+	String index(){
+		return "Hello Spring Boot";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NovelApplication.class, args);
